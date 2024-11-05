349f82fa7963086fe87b6f99561ec7d01d9ad72f
emmaxfox <emmafox1300@gmail.com>
2024-11-05 16:30:08 +0000

    Created LoanedInventory model

---
+++ models/LoanedInventory.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "loaned_inventory".
+ * 
+ * @property id $id
+ * @property new_prop_id $new_prop_id
+ * @property bl_code $bl_code
+ * @property date_borrowed $date_borrowed
+ * @property date_returned $date_returned
+ */
+class LoanedInventory extends \yii\db\ActiveRecord {
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName() {
+        return 'loaned_inventory';
+    }
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb() {
+        return Yii::$app->get('db');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules() {
+        // Check and see if this is right
+        return [
+            [['id'], 'integer', 'unique'],
+            [['new_prop_tag', 'bl_code', 'date_borrowed'], 'required'],
+            [['new_prop_tag'], 'integer'],
+            [['bl_code'], 'string', 'max' => 20],
+            [['date_borrowed', 'date_returned'], 'datetime']
+        ];
+    }
+
+    public function attributeLabels() {
+        // should be right
+        return [
+            'id' => 'ID',
+            'new_prop_tag' => 'New Prop Tag',
+            'bl_code' => 'Bl Code',
+            'date_borrowed' => 'Date Borrowed',
+            'date_returned' => 'Date Returned'
+        ];
+    }
+
+    /**
+     * Gets query for [[Inventory]].
+     * 
+     * @return \yii\db\ActiveQuery|InventoryQuery
+     */
+    public function getNewProp() {
+        return $this->hasMany(Inventory::class, ['new_prop_tag' => 'new_prop_tag']);
+    }
+
+    /**
+     * Gets query for [[BlCode]].
+     *
+     * @return \yii\db\ActiveQuery|LocationQuery
+     */
+    public function getBlCode() {
+        return $this->hasMany(Location::class, ['bl_code' => 'bl_code']);
+    }
+
+    /**
+     * Gets the date borrowed.
+     * 
+     * @return date The date this was borrowed
+     */
+    public function getDateBorrowed() {
+        return $this->date_borrowed;
+    }
+
+    /**
+     * Gets the date returned.
+     * 
+     * @return date The date this was returned
+     */
+    public function getDateReturned() {
+        return $this->date_returned;
+    }
+}
